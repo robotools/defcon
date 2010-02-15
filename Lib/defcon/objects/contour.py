d05e182c068cee56d79baec99cc115605a6f4f95
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2010-02-15 15:40:25 +0000

    Use the high level method.

--- Lib/defcon/objects/contour.py
+++ Lib/defcon/objects/contour.py
@@ -457,14 +457,14 @@ class Contour(BaseObject):
 
     def getDataToSerializeForUndo(self):
         data = dict(
-            points=[point.getDataToSerializeForUndo() for point in self._points]
+            points=[point.serializeForUndo() for point in self._points]
         )
         return data
 
     def loadDeserializedDataFromUndo(self, data):
         for pointData in data["points"]:
             point = self._pointClass((0, 0))
-            point.loadDeserializedDataFromUndo(pointData)
+            point.deserializeFromUndo(pointData)
             self._addPoint(point)
         self._setParentDataInPoints()
 
