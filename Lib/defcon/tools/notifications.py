e8efbe8973f4d63522e2485380c56ff625f08349
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2010-10-27 01:01:22 +0000

    Reworked the hold and disable functionality to make it more flexible.
    Frederik contributed some doctests. Thanks Frederik!

--- Lib/defcon/tools/notifications.py
+++ Lib/defcon/tools/notifications.py
@@ -78,80 +78,91 @@ class NotificationCenter(object):
         observableObj = observable
         if observable is not None:
             observable = weakref.ref(observable)
-        # disabled
-        if (observable, notification) in self._disabled:
-            return
-        if observable in self._disabled:
-            return
-        # held
-        if (observable, notification) in self._holds:
-            n = (notification, observable, data)
-            if not self._holds[observable, notification]["notifications"] or self._holds[observable, notification]["notifications"][-1] != n:
-                self._holds[observable, notification]["notifications"].append(n)
-            return
-        if observable in self._holds:
-            n = (notification, observable, data)
-            if not self._holds[observable]["notifications"] or self._holds[observable]["notifications"][-1] != n:
-                self._holds[observable]["notifications"].append(n)
-            return
+        # check for some known combinations in the holds and disableds
+        if self._holds or self._disabled:
+            possibilities = [
+                (None, None, None),
+                (observable, None, None),
+                (None, None, notification),
+                (observable, None, notification)
+            ]
+            for possibleObservable, possibleObserver, possibleNotification in possibilities:
+                key = self._makeHoldAndDisableKey(observable=possibleObservable, observer=possibleObserver, notification=possibleNotification)
+                if key in self._disabled:
+                    return
+                if key in self._holds:
+                    n = (notification, observable, data)
+                    if not self._holds[key]["notifications"] or self._holds[key]["notifications"][-1] != n:
+                        self._holds[key]["notifications"].append(n)
+                    return
         # post
         if notification in self._observables:
             for observableRef, observerDict in self._observables[notification].items():
-                if observable == observableRef:
+                if observable == observableRef or observable is None:
                     for observerRef, methodName in observerDict.items():
                         observer = observerRef()
-                        # disabled for observer
+                        # check the holds and disableds
                         if self._disabled:
-                            if (observableRef, notification, observerRef) in self._disabled:
-                                continue
-                            elif (observableRef, observerRef) in self._disabled:
-                                continue
-                            elif (notification, observerRef) in self._disabled:
+                            possibilities = [
+                                (None, observerRef, None),
+                                (observableRef, observerRef, None),
+                                (None, observerRef, notification),
+                                (observableRef, observerRef, notification),
+                            ]
+                            skip = False
+                            for key in possibilities:
+                                if key in self._disabled:
+                                    break
+                                    skip = True
+                                if key in self._holds:
+                                    n = (notification, observable, data)
+                                    if not self._holds[key]["notifications"] or self._holds[key]["notifications"][-1] != n:
+                                        self._holds[key]["notifications"].append(n)
+                                    skip = True
+                                    break
+                            if skip:
                                 continue
                         callback = getattr(observer, methodName)
                         n = Notification(notification, observableRef, data)
                         callback(n)
 
-    def holdNotificationsForObservable(self, observable, notification=None):
+    def holdNotifications(self, observable=None, notification=None):
         """
         Hold all notifications posted to all objects observing
         **notification** in **observable**.
 
-        * **observable** The object that the notification belongs to.
+        * **observable** The object that the notification belongs to. This is optional.
+          If no *observable* is given, *all* *notifications* will be held.
         * **notification** The name of the notification. This is optional.
           If no *notification* is given, *all* notifications for *observable*
           will be held.
 
-        Held notifications will be posted after the a matching *notification*
-        and *observable* have been passed to :meth:`Notification.releaseHeldNotificationsForObservable`.
+        Held notifications will be posted after the matching *notification*
+        and *observable* have been passed to :meth:`Notification.releaseHeldNotifications`.
         This object will retain a count of how many times it has been told to
         hold notifications for *notification* and *observable*. It will not
         post the notifications until the *notification* and *observable*
         have been released the same number of times.
         """
-        observable = weakref.ref(observable)
-        if notification is not None:
-            key = (observable, notification)
-        else:
-            key = observable
+        if observable is not None:
+            observable = weakref.ref(observable)
+        key = self._makeHoldAndDisableKey(observable=observable, notification=notification)
         if key not in self._holds:
             self._holds[key] = dict(holdCount=0, notifications=[])
         self._holds[key]["holdCount"] += 1
 
-    def releaseHeldNotificationsForObservable(self, observable, notification=None):
+    def releaseHeldNotifications(self, observable=None, notification=None):
         """
         Release all held notifications posted to all objects observing
         **notification** in **observable**.
 
-        * **observable** The object that the notification belongs to.
+        * **observable** The object that the notification belongs to. This is optional.
         * **notification** The name of the notification. This is optional.
         """
         observableObj = observable
-        observable = weakref.ref(observable)
-        if notification is not None:
-            key = (observable, notification)
-        else:
-            key = observable
+        if observable is not None:
+            observable = weakref.ref(observable)
+        key = self._makeHoldAndDisableKey(observable=observable, notification=notification)
         self._holds[key]["holdCount"] -= 1
         if self._holds[key]["holdCount"] == 0:
             notifications = self._holds[key]["notifications"]
@@ -159,24 +170,25 @@ class NotificationCenter(object):
             for notification, o, data in notifications:
                 self.postNotification(notification, observableObj, data)
 
-    def areNotificationsHeldForObservable(self, observable, notification=None):
+    def areNotificationsHeld(self, observable=None, notification=None):
         """
         Returns a boolean indicating if notifications posted to all objects observing
         **notification** in **observable** are being held.
 
-        * **observable** The object that the notification belongs to.
+        * **observable** The object that the notification belongs to. This is optional.
         * **notification** The name of the notification. This is optional.
         """
-        if (observable, notification) in self._holds:
-            return True
-        return weakref.ref(observable) in self._holds
+        if observable is not None:
+            observable = weakref.ref(observable)
+        return key in self._holds
 
-    def disableNotificationsForObservable(self, observable, notification=None, observer=None):
+    def disableNotifications(self, observable=None, notification=None, observer=None):
         """
         Disable all posts of **notification** from **observable** posted
         to **observer** observing.
 
-        * **observable** The object that the notification belongs to.
+        * **observable** The object that the notification belongs to. This is optional.
+          If no *observable* is given, *all* *notifications* will be disabled for *observer*.
         * **notification** The name of the notification. This is optional.
           If no *notification* is given, *all* notifications for *observable*
           will be disabled for *observer*.
@@ -189,57 +201,55 @@ class NotificationCenter(object):
         enable new notifications until the *notification* and *observable*
         have been released the same number of times.
         """
-        observable = weakref.ref(observable)
+        if observable is not None:
+            observable = weakref.ref(observable)
         if observer is not None:
             observer = weakref.ref(observer)
-        key = self._makeDisableKey(observable, notification=notification, observer=observer)
+        key = self._makeHoldAndDisableKey(observable, notification=notification, observer=observer)
         if key not in self._disabled:
             self._disabled[key] = 0
         self._disabled[key] += 1
 
-    def enableNotificationsForObservable(self, observable, notification=None, observer=None):
+    def enableNotifications(self, observable=None, notification=None, observer=None):
         """
         Enable notifications posted to all objects observing
         **notification** in **observable**.
 
-        * **observable** The object that the notification belongs to.
+        * **observable** The object that the notification belongs to. This is optional.
         * **notification** The name of the notification. This is optional.
         * **observer** The observer. This is optional.
         """
         observableObj = observable
-        observable = weakref.ref(observable)
+        if observable is not None:
+            observable = weakref.ref(observable)
         if observer is not None:
             observer = weakref.ref(observer)
-        key = self._makeDisableKey(observable, notification=notification, observer=observer)
+        key = self._makeHoldAndDisableKey(observable, notification=notification, observer=observer)
         self._disabled[key] -= 1
         if self._disabled[key] == 0:
             del self._disabled[key]
 
-    def areNotificationsDisabledForObservable(self, observable, notification=None, observer=None):
+    def areNotificationsDisabled(self, observable=None, notification=None, observer=None):
         """
         Returns a boolean indicating if notifications posted to all objects observing
         **notification** in **observable** are disabled.
 
-        * **observable** The object that the notification belongs to.
+        * **observable** The object that the notification belongs to. This is optional.
         * **notification** The name of the notification. This is optional.
         * **observer** The observer. This is optional.
         """
-        observable = weakref.ref(observable)
+        if observable is not None:
+            observable = weakref.ref(observable)
         if observer is not None:
             observer = weakref.ref(observer)
-        key = self._makeDisableKey(observable, notification=notification, observer=observer)
+        key = self._makeHoldAndDisableKey(observable, notification=notification, observer=observer)
         return key in self._disabled
 
-    def _makeDisableKey(self, observable, notification=None, observer=None):
+    def _makeHoldAndDisableKey(self, observable, notification=None, observer=None):
         key = [observable]
-        if notification is not None:
-            key.append(notification)
-        if observer is not None:
-            key.append(observer)
-        if len(key) == 1:
-            key = key[0]
-        else:
-            key = tuple(key)
+        key.append(notification)
+        key.append(observer)
+        key = tuple(key)
         return key
 
 
@@ -271,3 +281,146 @@ class Notification(object):
         return self._data
 
     data = property(_get_data, doc="Arbitrary data passed along with the notification. There is no set format for this data and there is not requirement that any data be present. Refer to the documentation for methods that are responsible for generating notifications for information about this data.")
+
+
+# -----
+# Tests
+# -----
+
+class _TestObserver(object):
+
+    def notificationCallback(self, notification):
+        print notification.name, notification.object.name
+
+
+class _TestObservable(object):
+
+    def __init__(self, center, name):
+        self.center = center
+        self.name = name
+
+    def postNotification(self, name):
+        self.center.postNotification(name, self)
+
+
+def _testAddObserver():
+    """
+    >>> center = NotificationCenter()
+    >>> observable = _TestObservable(center, "Observable")
+    >>> observer = _TestObserver()
+    >>> center.addObserver(observer, "notificationCallback", "A", observable)
+    >>> center.hasObserver(observer, "A", observable)
+    True
+    >>> center.hasObserver(observer, "B", observable)
+    False
+    """
+
+def _testRemoveObserver():
+    """
+    >>> center = NotificationCenter()
+    >>> observable = _TestObservable(center, "Observable")
+    >>> observer = _TestObserver()
+    >>> center.addObserver(observer, "notificationCallback", "A", observable)
+    >>> center.removeObserver(observer, "A", observable)
+    >>> center.hasObserver(observer, "A", observable)
+    False
+    """
+
+def _testPostNotification():
+    """
+    >>> center = NotificationCenter()
+    >>> observable = _TestObservable(center, "Observable")
+    >>> observer = _TestObserver()
+    >>> center.addObserver(observer, "notificationCallback", "A", observable)
+    >>> observable.postNotification("A")
+    A Observable
+    """
+
+def _testHoldNotifications():
+    """
+    >>> center = NotificationCenter()
+    >>> observable1 = _TestObservable(center, "Observable1")
+    >>> observable2 = _TestObservable(center, "Observable2")
+    >>> observer = _TestObserver()
+    >>> center.addObserver(observer, "notificationCallback", "A", observable1)
+    >>> center.addObserver(observer, "notificationCallback", "B", observable1)
+    >>> center.addObserver(observer, "notificationCallback", "C", observable2)
+
+    # hold all notifications
+    >>> center.holdNotifications()
+    >>> observable1.postNotification("A")
+    >>> observable1.postNotification("A")
+    >>> observable1.postNotification("B")
+    >>> observable2.postNotification("C")
+    >>> center.releaseHeldNotifications()
+    A Observable1
+    B Observable1
+    C Observable2
+
+    # hold all notifications of a specific observable
+    >>> center.holdNotifications(observable=observable1)
+    >>> observable1.postNotification("A")
+    >>> observable1.postNotification("A")
+    >>> observable1.postNotification("B")
+    >>> observable2.postNotification("C")
+    C Observable2
+    >>> center.releaseHeldNotifications(observable=observable1)
+    A Observable1
+    B Observable1
+
+    # hold all notifications of a specific notification
+    >>> center.holdNotifications(notification="A")
+    >>> observable1.postNotification("A")
+    >>> observable1.postNotification("A")
+    >>> observable1.postNotification("B")
+    B Observable1
+    >>> observable2.postNotification("C")
+    C Observable2
+    >>> center.releaseHeldNotifications(notification="A")
+    A Observable1
+    """
+
+def _testDisableNotifications():
+    """
+    >>> center = NotificationCenter()
+    >>> observable1 = _TestObservable(center, "Observable1")
+    >>> observable2 = _TestObservable(center, "Observable2")
+    >>> observer = _TestObserver()
+    >>> center.addObserver(observer, "notificationCallback", "A", observable1)
+    >>> center.addObserver(observer, "notificationCallback", "B", observable1)
+    >>> center.addObserver(observer, "notificationCallback", "C", observable2)
+
+    # disable all notifications
+    >>> center.disableNotifications()
+    >>> observable1.postNotification("A")
+    >>> observable1.postNotification("B")
+    >>> observable2.postNotification("C")
+    >>> center.enableNotifications()
+    >>> observable1.postNotification("A")
+    A Observable1
+
+    # disable all notifications for a specific observable
+    >>> center.disableNotifications(observable=observable1)
+    >>> observable1.postNotification("A")
+    >>> observable1.postNotification("B")
+    >>> observable2.postNotification("C")
+    C Observable2
+    >>> center.enableNotifications(observable=observable1)
+    >>> observable1.postNotification("A")
+    A Observable1
+
+    # disable all notifications for a specific notification
+    >>> center.disableNotifications(notification="A")
+    >>> observable1.postNotification("A")
+    >>> observable1.postNotification("B")
+    B Observable1
+    >>> observable2.postNotification("C")
+    C Observable2
+    >>> center.enableNotifications(notification="A")
+    >>> observable1.postNotification("A")
+    A Observable1
+    """
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
