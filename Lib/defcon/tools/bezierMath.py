1886adab4f7f0165a81d0138dbf1a5ad32902bcd
Frederik Berlaen <frederik@typemytype.com>
2013-07-18 23:07:41 +0200

    improving joinSegments

--- Lib/defcon/tools/bezierMath.py
+++ Lib/defcon/tools/bezierMath.py
@@ -2,8 +2,7 @@
 Contributed by Frederik Berlaen.
 """
 
-
-from math import sqrt
+from math import sqrt, atan2
 
 def _distance(xxx_todo_changeme, xxx_todo_changeme1):
     (x1, y1) = xxx_todo_changeme
@@ -41,7 +40,7 @@ def joinSegments(xxx_todo_changeme2, xxx_todo_changeme3, xxx_todo_changeme4, xxx
             x = on1X + (off2X - on1X) * factor
             y = on1Y + (off2Y - on1Y) * factor
         return (x, y), (off4X, off4Y), (on3X, on3Y)
-        
+
     if (on2X, on2Y) == (off3X, off3Y) and (off4X, off4Y) == (on3X, on3Y):
         ## last is a line segment
         d1 = _distance((on3X, on3Y), (off3X, off3Y))
@@ -53,35 +52,46 @@ def joinSegments(xxx_todo_changeme2, xxx_todo_changeme3, xxx_todo_changeme4, xxx
             x = on3X + (off3X - on3X) * factor
             y = on3Y + (off3Y - on3Y) * factor
         return (off1X, off1Y), (x, y), (on3X, on3Y)
-    
+
     if (off2X, off2Y) == (on2X, on2Y) == (off3X, off3Y) or (off2X, off2Y) == (on2X, on2Y) or (on2X, on2Y) == (off3X, off3Y):
         ## one or more bcps are on the joined point
         return (off1X, off1Y), (off4X, off4Y), (on3X, on3Y)
-    
+
     if (on1X, on1Y) == (off1X, off1Y):
         off1X = off1X + (off2X - off1X) * .1
         off1Y = off1Y + (off2Y - off1Y) * .1
-    
+
     if (on3X, on3Y) == (off4X, off4Y):
         off4X = off4X + (off3X - off4X) * .1
         off4Y = off4Y + (off3Y - off4Y) * .1
-    
+
+    smooth = False
+    if (off2X, off2Y) != (on2X, on2Y) and (off3X, off3Y) != (on2X, on2Y):
+        dx1, dy1 = on2X - off2X, on2Y - off2Y
+        dx2, dy2 = off3X - on2X, off3Y - on2Y
+        a1 = atan2(dx1, dy1)
+        a2 = atan2(dx2, dy2)
+        if abs(a1 - a2) < 0.05:
+            smooth = True
+
     # first calculate an aproximaly t
     d1 = _distance((on2X, on2Y), (off2X, off2Y))
     d2 = d1 + _distance((off3X, off3Y), (on2X, on2Y))
-    
+
     if d2 == 0:
         t = 0
     else:
         t = d1 / d2
-    
+
     # cut of the extreme t values
     error = .15
+    if smooth:
+        error = 0
     if t < error:
         t = error
     elif t > 1 - error:
         t = 1 - error
-    
+
     # just multiply the first handle of the first curve by t
     p2X = on1X + (off1X - on1X) * (1 / t)
     p2Y = on1Y + (off1Y - on1Y) * (1 / t)
