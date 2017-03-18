d7caa6b46a5c21bba505b8c7b86187d054412ff5
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-03-18 13:42:56 +0000

    Bump version: 0.2.5 → 0.2.6.dev0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="defcon",
-    version="0.2.5",
+    version="0.2.6.dev0",
     description="A set of flexible objects for representing UFO data.",
     long_description=long_description,
     author="Tal Leming",
