5998f5be11f9f21cb3a3a648f86fa9cf388a8c26
Ben Kiel <ben@typefounding.com>
2018-03-22 22:58:47 -0500

    Release 0.3.6

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ with open('README.rst', 'r') as f:
 
 setup_params = dict(
     name="defcon",
-    version="0.3.6.dev0",
+    version="0.3.6",
     description="A set of flexible objects for representing UFO data.",
     long_description=long_description,
     author="Tal Leming",
