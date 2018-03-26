f922b91e46a32612cfd599b15b7dbb0023e0627e
Cosimo Lupo <cosimo@anthrotype.com>
2018-03-26 11:47:39 +0100

    Bump version: 0.4.0 → 0.4.1.dev0

--- Lib/defcon/__init__.py
+++ Lib/defcon/__init__.py
@@ -3,7 +3,7 @@ A set of objects that are suited to being the basis
 of font development tools. This works on UFO files.
 """
 from __future__ import absolute_import
-version = __version__ = "0.4.0"
+version = __version__ = "0.4.1.dev0"
 
 from defcon.errors import DefconError
 
