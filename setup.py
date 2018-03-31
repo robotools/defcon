4cb41bc67f1146b1a26342521c27c3a76c10c50f
Ben Kiel <ben@typefounding.com>
2018-03-30 21:05:11 -0500

    Bump version: 0.5.0 → 0.5.1.dev0

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ with open('README.rst', 'r') as f:
 
 setup_params = dict(
     name="defcon",
-    version="0.5.0",
+    version="0.5.1.dev0",
     description="A set of flexible objects for representing UFO data.",
     long_description=long_description,
     author="Tal Leming",
